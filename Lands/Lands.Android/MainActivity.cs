cd87170796abea3ee3cdd98d9ada755a6891743e
calaraujo <calaraujo@hotmail.com>
2018-11-23 16:18:52 -0200

    Initial commit

---
+++ Lands/Lands.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿namespace Lands.Droid
+{
+    using Android.App;
+    using Android.Content.PM;
+    using Android.OS;
+    using FFImageLoading.Forms.Droid;
+
+    [Activity(Label = "Lands", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CachedImageRenderer.Init(true);
+            LoadApplication(new App());
+        }
+    }
+}
